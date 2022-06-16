8864ed5af6fb78abb92830125921afbe7aba3339
diego.ananias <diego.ananias@picpay.com>
2022-06-16 17:59:08 -0300

    chore: add jest env to eslint config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    roots: [
-      '/src'
-    ],
+    roots: ['<rootDir>/src'],
   
     clearMocks: true,
     coverageDirectory: 'coverage',
     coverageProvider: "v8",
-    collectCoverageFrom: ['/src/**/*.ts'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
     testEnvironment: 'node',
     transform: {
       '.+\\.ts$': 'ts-jest'  
